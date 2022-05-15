1872e58cae13be08b30414627e451ee21e451021
klingAle <alexander@kling-online.net>
2022-05-15 18:08:35 +0200

    added scenario 4
    changed plot description

--- run_different_standard_configurations.py
+++ run_different_standard_configurations.py
@@ -60,9 +60,10 @@ RESERVES DOUBLED """
 def unlimited_resources():
     """Originally: Figure 37 WORLD MODEL WITH  UNLIMITED RESSOURCES
     The Key Parameter here seems to be the nruf2, which is described on P.390 of Dynamics of Growth
-    in a finite World (DGFW) """
+    in a finite World (DGFW)
+    The most fitting value for NRUF2 seems to be 0.6"""
 
-    nruf2 = 0.7
+    nruf2 = 0.65
 
     params = {'lines.linewidth': '3'}
     rcParams.update(params)
@@ -81,10 +82,39 @@ def unlimited_resources():
                          [[0, 1], [0, 1e3], [0, 1e3], [0, 16e9], [0, 32]],
                          figsize=(7, 5),
                          grid=1,
-                         title="World Model with unlimited Natural Resources ----- Parameter nruf2 = {}".format(nruf2))
+                         title="World Model with unlimited Natural Resources \nParameter: nruf2 = {}".format(nruf2))
+    show()
+
+def unlimited_resources_pollution_control():
+    """Originally: Figure 39 WORLD MODEL WITH UNLIMITED RESSOURCES AND POLLUTION CONTROL
+    The Key Parameter here seems to be the ppgf2, which is described on P.428 of Dynamics of Growth
+    in a finite World (DGFW) """
+
+    nruf2 = 0.6
+    ppgf2 = 0.25
+
+    params = {'lines.linewidth': '3'}
+    rcParams.update(params)
+
+    world3 = w3.World3()
+    world3.init_world3_constants(nri=2e12, nruf1=1, nruf2=nruf2, ppgf2=ppgf2, ppgf1=1)
+    world3.init_world3_variables()
+    world3.set_world3_table_functions()
+    world3.set_world3_delay_functions()
+    world3.run_world3(fast=True)
+
+    plot_world_variables(world3.time,
+                         [world3.nrfr, world3.iopc, world3.fpc, world3.pop,
+                          world3.ppolx],
+                         ["NRFR", "IOPC", "FPC", "POP", "PPOLX"],
+                         [[0, 1], [0, 1e3], [0, 1e3], [0, 16e9], [0, 32]],
+                         figsize=(7, 5),
+                         grid=1,
+                         title="World Model with unlimited Natural Resources and Pollution Control \nParameter: nruf2 = {}; ppgf2 = {}".format(nruf2, ppgf2))
     show()
 
 if __name__ == '__main__':
     #unlimited_resources()
     #doubled_resources()
-    standard_run()
+    #standard_run()
+    unlimited_resources_pollution_control()
