2547ac6f8ef3dde2999bb39b19014f6d7b2b37e4
TimSchell98 <103997105+TimSchell98@users.noreply.github.com>
2022-08-23 11:11:54 +0200

    analyse

--- PyWorld3 Update/Analyse.py
+++ PyWorld3 Update/Analyse.py
@@ -8,19 +8,6 @@ import pandas as pd
 import time
 startTime = time.time()
 
-#Excel sheets für jede Variable die verglichen werden sollen
-wb = Workbook()
-res = wb.add_sheet("Results")
-var = wb.add_sheet("Eingangsparameter")
-pop = wb.add_sheet("Population")
-d = wb.add_sheet("Death per Year")
-b = wb.add_sheet("Birth per Year")
-fpc = wb.add_sheet("Food per Capita")
-ef = wb.add_sheet("Ecological Footprint")
-hwi = wb.add_sheet("Human Welfare Index")
-
-
-
 "Data decleration"
 ## Import Observed Data
 # Population
@@ -28,20 +15,11 @@ pop_data_big = pd.read_csv('Data_population.csv')
 pop_data = pop_data_big.iloc[0:1, 19:]              # Data from 1970 - 2021
 pop_data = np.transpose(pop_data.values.tolist())
 
-# Food per capita
-
 #berechnen der eingangsparameter "sprünge"
 sim_anzahl = 11
 start_val = 3
 end_val = 4
 delta = (end_val-start_val)/(sim_anzahl-1)
-
-#eingangs Variablen die verändert werden sollen
-def dcfsn_f(i, sim_anzahl):
-    
-    #hier können wir eine funktion einfügen die die eingangs parameter berechnen, dieser wird auch in der excel geschrieben
-    var.write(i+1, 0, start_val+i*delta)
-    return start_val+i*delta
 
 def compare(pyworld_data, real_data, sensivity_variable):
     """calculate Rate of Change, NRMSD (Branderhorst 2020)
@@ -79,12 +57,8 @@ def compare(pyworld_data, real_data, sensivity_variable):
     results = pd.concat([results_dvalue, results_droc, results_nrmsd], 
                         axis = 1)
     
-    results_array = [d_value_sum,d_roc_sum,nrmsd_sum]
     
-    return results_array
-
-#ausgelagerte initierung für die verbesserung der Laufzeit
-world3 = World3(dt = 1)
+    return results
 
 for i in range(0,sim_anzahl):
     print("\nSimulation" , end =": ")
@@ -94,7 +68,8 @@ for i in range(0,sim_anzahl):
     print("s")
 
     #simulation durchführen
-    world3.init_world3_constants(dcfsn = dcfsn_f(i, sim_anzahl))
+    world3 = World3(dt = 1)
+    world3.init_world3_constants(dcfsn = start_val+i*delta)
     world3.init_world3_variables()
     world3.set_world3_table_functions()
     world3.set_world3_delay_functions()
@@ -108,23 +83,9 @@ for i in range(0,sim_anzahl):
     print(world3.dcfsn)
     "Evaluation"
     #compare(pyworld_data, real_data, sensivity_variable):
-    results_array = compare(pop_py, pop_data, "population")
+    results = compare(pop_py, pop_data, "population")
     print("Results:", end = ": ")
-    print(results_array)
-    
-    #alle Werte der Vergleichsvariablen in Excel schrieben
-    for y in range(0,200):
-        pop.write(y+1, i+1, world3.pop[y])
-        d.write(y+1, i+1, world3.d[y])
-        b.write(y+1, i+1, world3.b[y])
-        fpc.write(y+1, i+1, world3.fpc[y])
-        ef.write(y+1, i+1, world3.ef[y])
-        hwi.write(y+1, i+1, world3.hwi[y])
-        
-
-
-#excel speichern
-wb.save('sim-values.xls')
+    print(results)
 
 executionTime = (time.time() - startTime)
 print('Execution time in seconds: ' + str(executionTime))
