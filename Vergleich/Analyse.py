0826019948a31aa89a6786435f17c81beacc5a91
rwillamo <103309587+rwillamo@users.noreply.github.com>
2022-08-31 14:38:11 +0200

    Analyse Erweiterung und Verbesserung
    - Namen der Funktionen ändern
    - richtige Index im Ergebnis
    - Gewichtung Abweichungen
    - automatische Index bei Dateneingabe

--- Vergleich/Analyse.py
+++ Vergleich/Analyse.py
@@ -14,19 +14,14 @@ from xlwt import Workbook
 from pyworld3 import World3
 from pyworld3.utils import plot_world_variables
 import pandas as pd
+from math import isnan
+
 
 import time
 startTime = time.time()
 
 "Analyse Einstelungen"
 #berechnen der eingangsparameter "Spruenge"
-
-"Data decleration"
-## Import Observed Data
-# Population
-pop_data_big = pd.read_csv('Data_population.csv')
-pop_data = pop_data_big.iloc[0:1, 19:]              # Data from 1970 - 2021
-pop_data = np.transpose(pop_data.values.tolist())
 
 #Einteilen des Simulationsrasters
 sim_anzahl = 11
@@ -41,7 +36,7 @@ year_min = 1900
 period  = year_max1 - year_min
 
 
-"Data decleration"
+"Data - measured"
 ## Import Observed Data
 # Population
 pop_data_big = pd.read_csv('Data_population.csv')
@@ -64,7 +59,7 @@ measured_data = pd.DataFrame(data = measured_data, columns = ['year', 'populatio
 
 
 "Functions"
-def compare_calculate(pyworld_data, real_data, sensivity_variable):
+def calculate_differences(pyworld_data, real_data, sensivity_variable):
     """calculate Rate of Change, NRMSD (Branderhorst 2020)
     """
     #init variables für compare funktion
@@ -101,39 +96,13 @@ def compare_calculate(pyworld_data, real_data, sensivity_variable):
                         axis = 1)
     
     
-    return results
+    return results  
 
-def one_constant():
-    #Ergebnis-Matrix in Schleife berechnen
-    #erste Zeile
-    world3 = World3(dt = sim_time_step, year_max=year_max)
-    world3 = World3(dt = sim_time_step, year_max = year_max)
-    world3.init_world3_constants()
-    world3.init_world3_variables()
-    world3.set_world3_table_functions()
-    world3.set_world3_delay_functions()
-    world3.run_world3(fast=False)
-    ## Define Pyworld Data 
-    #population
-    pop_py= world3.pop[60:122]                          # Data from 1960 - 2021
-    results_pop = compare_calculate(pop_py, pop_data, "standard")  #erste Zeile stadard
-    for i in range(0,sim_anzahl):
-        #simulation durchführen 
-        world3 = World3(dt = sim_time_step, year_max=year_max)
-        world3.init_world3_constants(dcfsn = start_val+i*delta)
-        world3.init_world3_variables()
-        world3.set_world3_table_functions()
-        world3.set_world3_delay_functions()
-        world3.run_world3(fast=False)
-        ## Define Pyworld Data 
-        #population
-        pop_py= world3.pop[60:122]                          # Data from 1960 - 2021
-        #Evaluation
-        #compare(pyworld_data, real_data, sensivity_variable):
-        results_pop = results_pop.append(compare_calculate(pop_py, pop_data, start_val+i*delta))
-    return results_pop   
-
-def multiple_data():
+def evaluate_results(results_pop, portion_pop, results_al, portion_al):
+    results_total = results_pop * portion_pop + results_al * portion_al
+    return  results_total
+    
+def analyse_measured_pyworld_data():
     #Simulation für erste Zeile - Standard
     world3 = World3(dt = sim_time_step, year_max=year_max)
     world3.init_world3_constants()
@@ -141,28 +110,39 @@ def multiple_data():
     world3.set_world3_table_functions()
     world3.set_world3_delay_functions()
     world3.run_world3(fast=False)
+    
     # Pyworld Data 
     pyworld_data = measured_data*0
     pyworld_data['year'] = year_data
     pyworld_data['population'] = world3.pop
     pyworld_data['arable_land'] = world3.al
-    # Results
-    # Calculation first step
-    pop_py   = pyworld_data.iloc[60:121,1:2].to_numpy()
-    pop_data = measured_data.iloc[60:121,1:2].to_numpy()
-    results_pop1 = compare_calculate(pop_py, pop_data, "standard")  #erste Zeile stadard 
     
-    al_py   = pyworld_data.iloc[61:118,2:3].to_numpy()
-    al_data = measured_data.iloc[61:118,2:3].to_numpy()
-    results_al = compare_calculate(al_py, al_data, 3)  #erste Zeile stadard 
+    # get index
+    index_pop_first = measured_data['population'].index.get_loc(measured_data['population'].first_valid_index())
+    index_pop_last = measured_data['population'].index.get_loc(measured_data['population'].last_valid_index())
     
-    results_data = np.zeros((1, 3))
-    #results_data[:,0] = np.arange(start_val, end_val+delta, delta)
+    index_al_first = measured_data['arable_land'].index.get_loc(measured_data['arable_land'].first_valid_index())
+    index_al_last = measured_data['arable_land'].index.get_loc(measured_data['arable_land'].last_valid_index())
+    
+    # data type for function calculate differences
+    pop_py   = pyworld_data.iloc[index_pop_first:index_pop_last,1:2].to_numpy()
+    pop_data = measured_data.iloc[index_pop_first:index_pop_last,1:2].to_numpy()
+    
+    al_py   = pyworld_data.iloc[index_al_first:index_al_last,2:3].to_numpy()
+    al_data = measured_data.iloc[index_al_first:index_al_last,2:3].to_numpy()
+    
+    
+    #results first step
+    results_pop = calculate_differences(pop_py, pop_data, "standard")  #erste Zeile stadard 
+    results_al = calculate_differences(al_py, al_data, 3)  #erste Zeile stadard 
+
+    results_data = np.zeros((1, 4))
     results_data[0,0] = 4
-    results_data[0,1] = results_pop1["NRMSD"]
+    results_data[0,1] = results_pop["NRMSD"]
     results_data[0,2] = results_al["NRMSD"]
-    
-    results_data = pd.DataFrame(data=results_data, columns = ['dcfs', 'population', 'arable_land'])
+    #Gewichtung
+    results_data[0,3] = evaluate_results(results_data[0,1],1,results_data[0,1],0)
+    results_data = pd.DataFrame(data=results_data, columns = ['dcfs', 'population', 'arable_land', 'total'])
     
     #Schleife für verschiedene Inputs
     for i in range(0,sim_anzahl):
@@ -177,22 +157,21 @@ def multiple_data():
         pyworld_data['population'] = world3.pop
         pyworld_data['arable_land'] = world3.al
         
-        pop_py   = pyworld_data.iloc[60:121,1:2].to_numpy()
-        pop_data = measured_data.iloc[60:121,1:2].to_numpy()
-        results_pop1 = (compare_calculate(pop_py, pop_data, start_val+i*delta))
+        # data type for function calculate differences
+        pop_py   = pyworld_data.iloc[index_pop_first:index_pop_last,1:2].to_numpy()
+        pop_data = measured_data.iloc[index_pop_first:index_pop_last,1:2].to_numpy()
+        results_pop = (calculate_differences(pop_py, pop_data, start_val+i*delta))
         
-        al_py   = pyworld_data.iloc[61:118,2:3].to_numpy()
-        al_data = measured_data.iloc[61:118,2:3].to_numpy()
-        results_al = (compare_calculate(al_py, al_data, start_val+i*delta))
+        al_py   = pyworld_data.iloc[index_al_first:index_al_last,2:3].to_numpy()
+        al_data = measured_data.iloc[index_al_first:index_al_last,2:3].to_numpy()
+        results_al = (calculate_differences(al_py, al_data, start_val+i*delta))
         
-        results_add = pd.DataFrame(data = {'dcfs':[start_val+i*delta], 'population': [results_pop1["NRMSD"]], 'arable_land':[results_al["NRMSD"]]}).astype(float)
-        results_data = results_data.append(results_add)
-        
-        #results_data[i+1,0] = start_val+i*delta
-        #results_data[i+1,1] = results_pop1["NRMSD"]
-        #results_data[i+1,2] = results_al["NRMSD"]
-        #results_data = pd.DataFrame(data=results_data, columns = ['dcfs', 'population', 'arable_land'])
-    
+        result_total = evaluate_results(results_pop['NRMSD'],1,results_pop['NRMSD'],0)
+        results_add = pd.DataFrame(data = {'dcfs':[start_val+i*delta], 'population': [results_pop["NRMSD"]],
+                                           'arable_land':[results_al["NRMSD"]], 'total': result_total}).astype(float)
+        #results_data = results_data.append(results_add, ignore_index=True)      #was ist besser?
+        results_data = pd.concat([results_data, results_add], ignore_index=True)
+
     return    results_data
   
 def improved_limits():
@@ -209,23 +188,31 @@ def improved_limits():
 "Run and print Results"
 #results_pop = one_constant()
 #print(results_pop)
-res1=multiple_data()
+res1=analyse_measured_pyworld_data()
 #res = improved_limits()
 print(res1)
 
 
 
+
+
+
+
 "TODO"
-# delete one_var next upload
 # inputs multiple constants
 # arable land correct data (not all countries in sum)
 # one csv with measured data 
-# Gewichtung Vergleichsparameter (optional)
+# Gewichtung Vergleichsparameter Eingabe oben (siehe erster Punkt)
 # Gewichtung Jahre (optional)
 # Gewichtung Abweichung (optional)
 # multiple data automatisieren
     # eigenständige parameter selektierung (was vorhanden) - Zeile: 
     # vergleichsvariablen zusammenfassen - Zeile:
-#4.0 als 'standard'
-#kommentare anfang checken!
+# kommentare anfang checken!
 
+# Clean Code
+    # Data sieh oben
+    # array / Dataframe anhängen (alles array oder alles !DataFrame!)
+    # use one DataFrame in function compare_differendes instead of single arrays 
+    # -> call function only once 
+    # fist / last index with DataFrame
