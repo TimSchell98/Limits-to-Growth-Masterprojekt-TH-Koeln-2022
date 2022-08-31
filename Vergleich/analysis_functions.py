1c8ebb74452064e1504ce0ae07743557492cd93c
klingAle <alexander@kling-online.net>
2022-08-31 15:48:51 +0200

    added roc and d_value functions

--- Vergleich/analysis_functions.py
+++ Vergleich/analysis_functions.py
@@ -1,41 +1,24 @@
-def calculate_metrics(pyworld_data, real_data, sensivity_variable):
-    """
-    inputs are two arrays of the same size and a string for naming the datafram
-    calculate Rate of Change, NRMSD (Branderhorst 2020)
-    """
-    # init variables für compare funktion
-    d_value_list = []
-    d_roc_list = []
-    nrmsd_list = []
-    n = len(pyworld_data)
+import numpy as np
 
-    for year_n in np.arange(0, n, 5):
-        # d_value
-        d_value = (pyworld_data[year_n - 1] - real_data[year_n - 1]) / real_data[year_n - 1]
-        d_value_list.append(d_value)
-        d_value_sum = sum(d_value_list)
+def calculate_d_value(model_data, empirical_data):
+    '''Function for the Formula vor delta value
+    Originally from Herrington 2020 - Update to limits to growth
+    inputs are two arrays with data
+    Output is the calculated value different for the last timestep as specified in the paper
+    '''
+    d_value = (model_data[-1]-empirical_data[-1])/empirical_data[-1]
+    return d_value
 
-        # roc
-        d_roc = (((pyworld_data[year_n - 1] - pyworld_data[year_n - 6]) - (
-                    real_data[year_n - 1] - real_data[year_n - 6]))
-                 / (real_data[year_n - 1] - real_data[year_n - 6]))
-        d_roc_list.append(d_roc)
-        d_roc_sum = sum(d_roc_list)
+def calculate_roc(model_data, empirical_data, timestep, calculation_interval = 5):
+    k = calculation_interval * timestep
+    roc = ((model_data[-1]-model_data[-k])-(empirical_data[-1]-empirical_data[-k]))/(empirical_data[-1]-empirical_data[-k])
+    return roc
 
-        # nrmsd
-        nrmsd = (((((pyworld_data[year_n - 1] - real_data[year_n - 1]) ** 2) / 6)) ** (1 / 2)) / (
-                    real_data[year_n - 1] / 6)
-        nrmsd_list.append(nrmsd)
-        nrmsd_sum = sum(nrmsd_list)
 
-    results_dvalue = pd.DataFrame(data=d_value_sum, index=[sensivity_variable],
-                                  columns=['d value'])
-    results_droc = pd.DataFrame(data=d_roc_sum, index=[sensivity_variable],
-                                columns=['d ROC'])
-    results_nrmsd = pd.DataFrame(data=nrmsd_sum, index=[sensivity_variable],
-                                 columns=['NRMSD'])
-
-    results = pd.concat([results_dvalue, results_droc, results_nrmsd],
-                        axis=1)
-
-    return results
+if __name__=='__main__':
+    #testing roc and d_value caclulation
+    a = np.arange(10)
+    b = np.arange(0, 20, 2)
+    print(a,b)
+    print(calculate_d_value(b, a))
+    print(calculate_roc(b, a, 1, 5))
