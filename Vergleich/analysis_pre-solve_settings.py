d9b422e7ea21aa3a711f0bb76a79a024df6f32ed
klingAle <alexander@kling-online.net>
2022-10-12 15:43:11 +0200

    created analysis_pre-solve.py + settings file

---
+++ Vergleich/analysis_pre-solve_settings.py
@@ -0,0 +1,19 @@
+'''
+Settings File for the Pre-Solver Script
+'''
+import analysis_parallel_settings as s
+
+search_range_from_default = 0.25
+grid_resolution = 8
+
+parameter_default_dict = {
+    s.parameter1_name: s.parameter1_default,
+    s.parameter2_name: s.parameter2_default,
+    s.parameter3_name: s.parameter3_default
+}
+
+parameter_number_dict = {
+    s.parameter1_name: 1,
+    s.parameter2_name: 2,
+    s.parameter3_name: 3
+}
