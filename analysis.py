5e2d9246e9f44905d0d5c6ee47f5718891e64800
klingAle <alexander@kling-online.net>
2022-12-14 17:39:24 +0100

    deleted plotting in main script, added plot function call

--- analysis.py
+++ analysis.py
@@ -4,6 +4,7 @@ import numpy as np
 import multiprocessing as mp
 import analysis_parallel_settings_working as s
 import analysis_functions_working as af
+from analysis_plotting import plot_data
 import matplotlib.pyplot as plt
 import time
 from datetime import datetime
@@ -71,97 +72,7 @@ if __name__ == '__main__':
             metrics = pd.concat([metrics, metric_result])
             
     #   -   -   - Plot data -   -   -
-        
-        #ToDo in eine funktion packen
-        #plot population
-        for i in range(0,parameter_list_full.shape[0]):
-            df_results['pop_{}'.format(i)].plot(legend=0, color = ["b"], linewidth = 0.4)
-        empirical_data["Population"].plot(legend=0, color = ["r"], linewidth = 1.5)
-        plt.title('Population')
-        plt.ylim([1e9,10e9])
-        plt.xlim([0,122])
-        plt.show()
-        
-        #plot death rate
-        for i in range(0,parameter_list_full.shape[0]):
-            df_results['cdr_{}'.format(i)].plot(legend=0, color = ["b"], linewidth = 0.4)
-        empirical_data["Death_rate"].plot(legend=0, color = ["r"], linewidth = 1.5)
-        plt.title('Death Rate')
-        plt.ylim([5,25])
-        plt.xlim([0,122])
-        plt.show()  
-        
-        #plot birth rate
-        for i in range(0,parameter_list_full.shape[0]):
-            df_results['cbr_{}'.format(i)].plot(legend=0, color = ["b"], linewidth = 0.4)
-        empirical_data["Birth_rate"].plot(legend=0, color = ["r"], linewidth = 1.5)
-        plt.title('Birth Rate')
-        plt.ylim([5,55])
-        plt.xlim([0,122])
-        plt.show() 
-        
-        #plot food per capita 
-        for i in range(0,parameter_list_full.shape[0]):
-            df_results['fpc_{}'.format(i)].plot(legend=0, color = ["b"], linewidth = 0.4)
-        empirical_data["Food_per_capita_ve"].plot(legend=0, color = ["r"], linewidth = 1.5)
-        plt.title('Food per Capita')
-        plt.ylim([0,1000])
-        plt.xlim([0,122])
-        plt.show() 
-        
-        #plot Pollution_proportion
-        for i in range(0,parameter_list_full.shape[0]):
-            df_results['pp_dtp_{}'.format(i)].plot(legend=0, color = ["b"], linewidth = 0.4)
-        empirical_data["Pollution_proportion"].plot(legend=0, color = ["r"], linewidth = 1.5)
-        plt.title('Pollution')
-        plt.ylim([-1,2])
-        plt.xlim([0,122])
-        plt.show() 
-        
-        #plot Expected_years_of_schooling_proportion
-        for i in range(0,parameter_list_full.shape[0]):
-            df_results['sopcp_{}'.format(i)].plot(legend=0, color = ["b"], linewidth = 0.4)
-        empirical_data["Expected_years_of_schooling_proportion"].plot(legend=0, color = ["r"], linewidth = 1.5)
-        plt.title('Expected years of schooling')
-        plt.ylim([-0.25,0.25])
-        plt.xlim([0,122])
-        plt.show() 
-        
-        #plot Fossil_fuel_consumption_proportion
-        for i in range(0,parameter_list_full.shape[0]):
-            df_results['nrurp_{}'.format(i)].plot(legend=0, color = ["b"], linewidth = 0.4)
-        empirical_data["Fossil_fuel_consumption_proportion"].plot(legend=0, color = ["r"], linewidth = 1.5)
-        plt.title('Fossil fuel consumption')
-        plt.ylim([-0.25,0.25])
-        plt.xlim([0,122])
-        plt.show() 
-        
-        #plot IPP
-        for i in range(0,parameter_list_full.shape[0]):
-            df_results['iop_{}'.format(i)].plot(legend=0, color = ["b"], linewidth = 0.4)
-        empirical_data["IPP_USA_proportion"].plot(legend=0, color = ["r"], linewidth = 1.5)
-        plt.title('IPP')
-        plt.ylim([-0.40,0.40])
-        plt.xlim([0,122])
-        plt.show() 
-        
-        #plot EF
-        for i in range(0,parameter_list_full.shape[0]):
-            df_results['ef_{}'.format(i)].plot(legend=0, color = ["b"], linewidth = 0.4)
-        empirical_data["Ecological_Footprint"].plot(legend=0, color = ["r"], linewidth = 1.5)
-        plt.title('Ecological Footprint (not in NRMSD total!)')
-        plt.ylim([0,5])
-        plt.xlim([0,122])
-        plt.show()
-        
-        #plot HWI
-        for i in range(0,parameter_list_full.shape[0]):
-            df_results['hwi_{}'.format(i)].plot(legend=0, color = ["b"], linewidth = 0.4)
-        empirical_data["Human_Welfare"].plot(legend=0, color = ["r"], linewidth = 1.5)
-        plt.title('Human Welfare Index (not in NRMSD total!)')
-        plt.ylim([0,1])
-        plt.xlim([0,122])
-        plt.show()
+        plot_data(df_results, empirical_data, parameter_list_full)
     
         #   -   -   - Calculate next parameter_list_full and save values in list -   -   -
         print(str(s.variable_to_improve) + " min = " + str(round(metrics[s.variable_to_improve].min(),8)))
