998ec4840a69c792b201dbff36862de126cf47cf
klingAle <alexander@kling-online.net>
2023-03-08 17:24:10 +0100

    created BAU Background Plots

--- result_plotting.py
+++ result_plotting.py
@@ -10,7 +10,62 @@ Copied plotting function from the original PyWorld3 Package to make Changes in t
 
 '''
 
+def plot_world_variables_black_linestyle(time, var_data, var_names, var_lims, alpha, linestyle,
+                         img_background=None,
+                         title=None,
+                         figsize=None,
+                         dist_spines=0.09,
+                         grid=False):
+    """
+    Plots world state from an instance of World3 or any single sector.
 
+    """
+    prop_cycle = plt.rcParams['axes.prop_cycle']
+    colors = prop_cycle.by_key()['color']
+
+    var_number = len(var_data)
+
+    fig, host = plt.subplots(figsize=figsize)
+    axs = [host, ]
+    for i in range(var_number-1):
+        axs.append(host.twinx())
+
+    fig.subplots_adjust(left=dist_spines*2)
+
+
+    if img_background is not None:
+        im = imread(img_background)
+        axs[0].imshow(im, aspect="auto",
+                      extent=[time[0], time[-1],
+                              var_lims[0][0], var_lims[0][1]], cmap="gray")
+
+    ps = []
+    for ax, label, ydata, linestyle, alpha in zip(axs, var_names, var_data, linestyle, alpha):
+        ps.append(ax.plot(time, ydata, label=label, color = 'black', linestyle=linestyle, alpha=alpha)[0])
+    axs[0].grid(grid)
+    axs[0].set_xlim(time[0], time[-1])
+
+    for ax, lim in zip(axs, var_lims):
+        ax.set_ylim(lim[0], lim[1])
+
+    for ax_ in axs:
+        formatter_ = EngFormatter(places=0, sep="\N{THIN SPACE}")
+        ax_.tick_params(axis='y', rotation=90)
+        ax_.yaxis.set_major_locator(plt.MaxNLocator(5))
+        ax_.yaxis.set_major_formatter(formatter_)
+        #ax_.legend()
+
+    tkw = dict(size=4, width=1.5)
+    axs[0].set_xlabel("time [years]")
+    axs[0].tick_params(axis='x', **tkw)
+
+    fig.legend(loc='upper right', bbox_to_anchor=(0.3, 0.75))
+
+    if title is not None:
+        #fig.suptitle(title, x=0.95, ha="right", fontsize=10)
+        fig.suptitle(title, fontsize=14)
+
+    #plt.tight_layout()
 
 def plot_world_variables(time, var_data, var_names, var_lims, alpha,
                          img_background=None,
@@ -64,14 +119,15 @@ def plot_world_variables(time, var_data, var_names, var_lims, alpha,
     axs[0].set_xlabel("time [years]")
     axs[0].tick_params(axis='x', **tkw)
     for i, (ax, p) in enumerate(zip(axs, ps)):
-        ax.set_ylabel(p.get_label(), rotation="vertical")
+        ax.set_ylabel(p.get_label(), rotation="vertical", fontsize='medium', horizontalalignment='right')
         ax.yaxis.label.set_color(p.get_color())
         ax.tick_params(axis='y', colors=p.get_color(), **tkw)
-        ax.yaxis.set_label_coords(-i*dist_spines, 1.01)
+        ax.yaxis.set_label_coords(-i*dist_spines-0.05, 1)
 
 
     if title is not None:
-        fig.suptitle(title, x=0.95, ha="right", fontsize=10)
+        #fig.suptitle(title, x=0.95, ha="right", fontsize=10)
+        fig.suptitle(title, fontsize=14)
 
     plt.tight_layout()
 
